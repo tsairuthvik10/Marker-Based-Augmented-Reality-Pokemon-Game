94e16f29071dfb512fca0c20047f8950b683883f
T Sai Ruthvik <f2013489@goa.bits-pilani.ac.in>
2017-09-10 02:01:31 +0530

    ar game

---
+++ Assets/pokemon1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class pokemon1 : MonoBehaviour {
+	public Button attackbutton1;
+	public GameObject hose;
+	public float totalhp1=100;
+	public Scrollbar Healthbar;
+	
+	// Use this for initialization
+	void Start () {
+		attackbutton1.onClick.AddListener (attackbutton1down);
+		hose.transform.FindChild ("Glow").gameObject.SetActive (false);
+		hose.transform.gameObject.SetActive (false);
+	}
+	
+	IEnumerator Wait (){
+		hose.transform.FindChild ("Glow").gameObject.SetActive (true);
+		hose.transform.gameObject.SetActive (true);
+		yield return new WaitForSeconds (2);
+		hose.transform.FindChild ("Glow").gameObject.SetActive (false);
+		hose.transform.gameObject.SetActive (false);
+	}
+	
+	void attackbutton1down (){
+		StartCoroutine (Wait ());
+		
+		
+	}
+	
+	public void Damage(float value)
+	{
+		totalhp1 -= value;
+		Healthbar.size = totalhp1/ 100f;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/pokemon2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class pokemon2 : MonoBehaviour {
+	public Button attackbutton2;
+	public GameObject fire;
+		public float totalhp2=100;
+	public Scrollbar Healthbar1;
+	// Use this for initialization
+	void Start () {
+		attackbutton2.onClick.AddListener (attackbutton2down);
+		fire.transform.FindChild ("Glow").gameObject.SetActive (false);
+		fire.transform.gameObject.SetActive (false);
+	}
+	
+	IEnumerator Wait (){
+		fire.transform.FindChild ("Glow").gameObject.SetActive (true);
+		fire.transform.gameObject.SetActive (true);
+		yield return new WaitForSeconds (2);
+		fire.transform.FindChild ("Glow").gameObject.SetActive (false);
+		fire.transform.gameObject.SetActive (false);
+	}
+	
+	void attackbutton2down (){
+		StartCoroutine (Wait ());
+		
+		
+	}
+	
+		public void Damage(float value)
+	{
+		totalhp2 -= value;
+		Healthbar1.size = totalhp2/ 100f;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
